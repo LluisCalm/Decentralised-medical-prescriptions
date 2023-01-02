038fc5896017afb062e5e43b14eee1483f64e92e
LluisCalm <sanchezcalm.lluis@gmail.com>
2023-01-02 11:14:03 +0100

    Comments on patient.sol

--- patient.sol
+++ patient.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// @title Contracte que defineix els pacients i tot el que poden fer dins el projecte de digitalitzar el sistema de receptres sanitàries 
+// @title Contracte pels pacients
 // @author Lluis Sánchez Calm
+// @notice S'encarrega de gestionar totes les accions que poden utilitzar els pacients
+
 
 import "./hospital.sol";
 import "./prescription.sol";
@@ -17,6 +19,7 @@ contract PatientContract {
     PrescriptionNFT prescription;
     address prescriptionAddress;
 
+    // @param la direccio de l'adreça de l'hospital i la del token ERC721
     constructor(address _hospitalAddress, address _prescriptionAddress) public {
         hospital = Hospital(_hospitalAddress);
         prescription = PrescriptionNFT(_prescriptionAddress);
@@ -28,20 +31,20 @@ contract PatientContract {
     //      MODIFIERS
     ////////////////////////////////
 
+    // @dev comprova si una direcció pertany a una farmàcia
     modifier toPharmacy (address _address) {
         require(hospital.isPharmacy(_address), "Destinatary not a pharmacy");
         _;
     }
 
-    modifier ifAddressAvailable (address _address){
-        require(!hospital.isPatient(_address));
-        _;
-    }
 
     ////////////////////////////////
     //      FUNCTIONS
     ////////////////////////////////
 
+    // @dev envia la recepta a la farmàcia
+    // @param direcció de la farmàcia i identificador del token
+    // @notice s'ha de donar permís al contracte del token per gestionar aquesta recepta
     function sendPrescription (address _pharmacyAddress, uint _tokenID) public toPharmacy(_pharmacyAddress){
         prescription.transferPrescription(_tokenID, _pharmacyAddress);
         prescription.changeState(_tokenID, "used");
